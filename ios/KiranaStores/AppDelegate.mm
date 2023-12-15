9c83a165ebdcebafb1fd0328a0e5231cc68feb76
bhavinkhatri <khatribhavin30@gmail.com>
2023-12-15 16:25:40 +0530

    - ios project setup - app icons changes android & ios

--- ios/KiranaStores/AppDelegate.mm
+++ ios/KiranaStores/AppDelegate.mm
@@ -6,7 +6,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  self.moduleName = @"sicu_aura";
+  self.moduleName = @"KiranaStores";
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
